588efbab99c8ff8023ca474a8369faf1ea690e5e
0x0384 <0x0384@gmail.com>
2023-02-04 03:43:08 -0600

    added validation to user input

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,13 @@ class User < ApplicationRecord
     # USes the password_digest column to add a password and password confirmation to our user.
     #It hashes the password so that it can be saved to the database.
     has_secure_password
+
+    # This checks to see if there is email input before it saves it
+    validates :email, presence: true
 end
+
+# Whenever you make changes to your model, you have to run the lines below to undo the migration and add the new instructions.
+# rails db:rollback
+# rails db:migrate
+#
+# Or, you can run 'rails db:migrate:redo' instead of the 2 lines above
